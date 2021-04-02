6cfd3319e449058f76dca7254da2ea12a0028583
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-04-02 11:23:43 -0700

    Add files via upload

---
+++ clk_divider.v
@@ -0,0 +1,29 @@
+
+module clk_divider
+#(parameter
+    integer DIV = 2
+)(
+    input wire clk_in,
+    input wire rst,
+    output reg clk_out
+);
+
+    localparam integer TC = (DIV / 2) - 1;
+    integer count = 0;
+
+    wire terminate = (count == TC);
+
+    always @( posedge clk_in )
+    begin
+        if (rst || terminate)
+            count <= 0;
+        else
+            count <= count + 1;
+        
+        if (rst)
+            clk_out <= 0;
+        else if (terminate)
+            clk_out <= ~clk_out;
+    end
+
+endmodule // clk_divider
