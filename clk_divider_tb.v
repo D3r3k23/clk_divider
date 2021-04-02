ea3b137816d6e13e66ecf61ab0f7dfff52dfb429
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-04-02 11:33:32 -0700

    Update clk_divider_tb.v

--- clk_divider_tb.v
+++ clk_divider_tb.v
@@ -1,5 +1,5 @@
 
-`timescale 1ns / 1ps
+`timescale 1us / 1ns
 
 module clk_divider_tb();
 
@@ -7,25 +7,25 @@ module clk_divider_tb();
     reg  rst;
     wire clk_out;
 
-    // 100 MHz clock gen
-    initial   clk_in = 1'b0;
-    always #5 clk_in <= ~clk_in;
+    // 1 kHz clock gen
+    initial     clk_in = 1'b0;
+    always #1ms clk_in <= ~clk_in;
 
-    clk_divider dut
+    clk_divider #( .DIV(4) )
+    DUT
     (
-        .clk_in  ( clk_in   ),
-        .rst     ( rst      ),
-        .clk_div ( 1000     ),
-        .clk_out ( clk_out  )  // 100 kHz
+        .clk_in  ( clk_in  ), // 1000 hz
+        .rst     ( rst     ),
+        .clk_out ( clk_out )  // 250 hz
     );
 
     initial begin : test
 
         rst = 1'b1;
-        #50
+        #100
         rst = 1'b0;
         
-        #80000
+        #40ms
         $finish;
 
     end // test
